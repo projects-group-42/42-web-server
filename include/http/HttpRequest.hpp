3b45720a38270efac4e1cd8bdbd0b12cf24de5bd
Daniel Jesus <dev.danieljs@gmail.com>
2026-06-12 02:59:07 -0300

    feat: add HttpRequest data model

--- include/http/HttpRequest.hpp
+++ include/http/HttpRequest.hpp
@@ -6,7 +6,7 @@
 /*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 22:20:40 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/06/11 22:43:47 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/06/12 02:54:28 by dajesus-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define HTTP_REQUEST_HPP
 
 # include <string>
+# include <map>
 
 class HttpRequest
 {
@@ -22,7 +23,7 @@ class HttpRequest
 		std::string							_uri;
 		std::string							_query;
 		std::string							_version;
-		std::string							_headers;
+		std::map<std::string, std::string>	_headers;
 		std::string							_body;
 
 	public:
@@ -30,6 +31,23 @@ class HttpRequest
 		HttpRequest(const HttpRequest &copy);
 		HttpRequest& operator=(const HttpRequest &other);
 		~HttpRequest(void);
+
+		const std::string&	getMethod(void) const;
+		const std::string&	getUri(void) const;
+		const std::string&	getQuery(void) const;
+		const std::string&	getVersion(void) const;
+		const std::map<std::string, std::string>& getHeaders(void) const;
+		const std::string&	getBody(void) const;
+
+		void	setMethod(const std::string &method);
+		void	setUri(const std::string &uri);
+		void	setQuery(const std::string &query);
+		void	setVersion(const std::string &version);
+		void	addHeader(const std::string &key, const std::string &value);
+		void	setBody(const std::string &body);
+
+		bool	hasHeader(const std::string &key) const;
+		std::string getHeader(const std::string &key) const;
 };
 
 #endif
--- src/http/HttpRequest.cpp
+++ src/http/HttpRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 22:21:02 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/06/11 22:43:40 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/06/12 02:55:06 by dajesus-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,75 @@ HttpRequest::~HttpRequest(void)
 {
 }
 
+const std::string& HttpRequest::getMethod(void) const
+{
+	return (_method);
+}
+
+const std::string& HttpRequest::getUri(void) const
+{
+	return (_uri);
+}
+
+const std::string& HttpRequest::getQuery(void) const
+{
+	return (_query);
+}
+
+const std::string& HttpRequest::getVersion(void) const
+{
+	return (_version);
+}
+
+const std::map<std::string, std::string>& HttpRequest::getHeaders(void) const
+{
+	return (_headers);
+}
+
+const std::string& HttpRequest::getBody(void) const
+{
+	return (_body);
+}
+
+void	HttpRequest::setMethod(const std::string &method)
+{
+	_method = method;
+}
+
+void	HttpRequest::setUri(const std::string &uri)
+{
+	_uri = uri;
+}
+
+void	HttpRequest::setQuery(const std::string &query)
+{
+	_query = query;
+}
+
+void	HttpRequest::setVersion(const std::string &version)
+{
+	_version = version;
+}
+
+void	HttpRequest::addHeader(const std::string &key, const std::string &value)
+{
+	_headers[key] = value;
+}
+
+void	HttpRequest::setBody(const std::string &body)
+{
+	_body = body;
+}
+
+bool	HttpRequest::hasHeader(const std::string &key) const
+{
+	return (_headers.find(key) != _headers.end());
+}
+
+std::string HttpRequest::getHeader(const std::string &key) const
+{
+	std::map<std::string, std::string>::const_iterator it = _headers.find(key);
+	if (it != _headers.end())
+		return (it->second);
+	return ("");
+}

59818ae3ad2220d59581f3dd8c0e90fea20e57c5
Daniel Jesus <dev.danieljs@gmail.com>
2026-06-12 22:33:35 -0300

    feat: add MIME type resolver by extension

--- src/http/MimeType.cpp
+++ src/http/MimeType.cpp
@@ -6,11 +6,13 @@
 /*   By: dajesus- <dajesus-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 19:55:24 by dajesus-          #+#    #+#             */
-/*   Updated: 2026/06/12 21:08:05 by dajesus-         ###   ########.fr       */
+/*   Updated: 2026/06/12 22:30:20 by dajesus-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "http/MimeType.hpp"
+# include "utils/Utils.hpp"
+# include <map>
 
 static std::map<std::string, std::string> buildMap(void)
 {
@@ -37,5 +39,14 @@ static std::map<std::string, std::string> buildMap(void)
 
 std::string	mimeType_resolve(const std::string &filePath)
 {
+	static const std::map<std::string, std::string> map = buildMap();
+	const std::string::size_type dotPos = filePath.rfind('.');
+
+	if (dotPos == std::string::npos || dotPos == 0 || dotPos + 1 == filePath.size())
+		return ("application/octet-stream");
+	std::string ext = toLower(filePath.substr(dotPos + 1));
+	std::map<std::string, std::string>::const_iterator it = map.find(ext);
+	if (it != map.end())
+		return (it->second);
 	return ("application/octet-stream");
 }
